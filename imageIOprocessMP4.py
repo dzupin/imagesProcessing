c15121d4330518d0d85ee6e490406c804a589329
dzupin <dzupinr@gmail.com>
2017-11-26 09:50:59 +0100

    Added sample mp4 for testing

--- imageIOprocessMP4.py
+++ imageIOprocessMP4.py
@@ -1,9 +1,9 @@
 import imageio
 
-reader = imageio.get_reader('Samtime.mp4')
+reader = imageio.get_reader('Sample.mp4')
 fps = reader.get_meta_data()['fps']
 
-writer = imageio.get_writer('Samtime_gray.mp4', fps=fps)
+writer = imageio.get_writer('Sample_gray.mp4', fps=fps)
 
 for im in reader:
     writer.append_data(im[:, :, 1])
